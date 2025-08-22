2f7263da9b074fd80655cdbd16bd825584209a22
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-08-22 16:43:28 -0700

    fix whitespace bull

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-      />{" "}
+      />
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased flex flex-1`}
       >
