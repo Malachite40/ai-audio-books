b87483226629a8ff41abbf26be27178f45f391cb
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-08-25 23:37:55 -0700

    header

--- apps/web/components/header.tsx
+++ apps/web/components/header.tsx
@@ -30,7 +30,7 @@ export function Header(props: HeaderProps) {
   const { data: subscriptionData } = api.subscriptions.self.useQuery();
 
   return (
-    <div className="h-12 flex w-full justify-between items-center px-4 border-b border-border top-0 sticky">
+    <div className="h-12 flex w-full justify-between items-center px-4 border-b border-border top-0 sticky bg-background">
       <Link href={"/"} className="flex gap-2 justify-center items-center">
         <Logo className="size-10" />
         {userData && (
