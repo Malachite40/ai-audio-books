39f8c6ecc866cc62e6349efff042e84d8e0be023
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-08-27 21:52:58 -0700

    audio current time

--- packages/trpc/src/routers/audioFileSettings.ts
+++ packages/trpc/src/routers/audioFileSettings.ts
@@ -5,8 +5,6 @@ export const audioFileSettingsRouter = createTRPCRouter({
   upsert: authenticatedProcedure
     .input(z.object({ id: z.string().uuid(), currentTime: z.number().min(0) }))
     .mutation(async ({ input, ctx }) => {
-      // For now, just create a blank settings row if one doesn't exist.
-      // We can add more fields later.
       await ctx.db.audioFileSettings.upsert({
         where: {
           audioFileId_userId: {
