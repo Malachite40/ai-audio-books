a265f7cdd4581309c24337ce275f349fcd2d39c9
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-08-31 12:28:52 -0700

    meta

--- apps/web/app/(app)/audio-file/[id]/page.tsx
+++ apps/web/app/(app)/audio-file/[id]/page.tsx
@@ -3,6 +3,7 @@ import { api } from "@/trpc/server";
 import { buttonVariants } from "@workspace/ui/components/button";
 import { cn } from "@workspace/ui/lib/utils";
 import { AudioLinesIcon } from "lucide-react";
+import { Metadata, ResolvingMetadata } from "next";
 import Link from "next/link";
 import ViewAudioClient from "./view.audio.client";
 
@@ -13,6 +14,37 @@ type tParams = Promise<{
 type tSearchParams = Promise<{
   page?: number;
 }>;
+
+export async function generateMetadata(
+  { params }: { params: tParams },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const { id } = await params;
+
+  // fetch data
+  const { audioFile } = await api.audio.fetch({ id });
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  if (!audioFile) {
+    return {
+      title: "Audio file not found",
+      openGraph: {
+        images: [],
+      },
+    };
+  }
+
+  return {
+    title: audioFile.name || "Audio File",
+    description: audioFile.text || undefined,
+    openGraph: {
+      images: previousImages,
+    },
+  };
+}
 
 export default async function Home(props: {
   params: tParams;
