db8d55c24f01f126dae8d7a97640d2b9743d0036
Dylan Cronkhite <dylancronkhite1@gmail.com>
2025-08-31 20:23:29 -0700

    meta

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,16 @@
 import { api } from "@/trpc/server";
+import { Metadata } from "next";
 import { HomeClient } from "./home.client";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Instant Audio Online",
+    description: "Create and listen to audio books instantly. Affordably.",
+    openGraph: {
+      images: ["/logo.png"],
+    },
+  };
+}
 
 export default async function Home() {
   const { kv } = await api.kv.getByKey({ key: "home-audio-file-id" });
